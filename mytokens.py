f0f5d89243a9f9da5d8f812f6c5c62fd0c71eac8
Tuc Tran Anh <intern.tanhtuc1@tma.com.vn>
2023-04-24 10:18:41 +0700

    subword tokenizer of pretrain bert

---
+++ mytokens.py
@@ -0,0 +1,28 @@
+#From hugging face
+from transformers import AutoTokenizer
+checkpoint = "distilbert-base-uncased-finetuned-sst-2-english"
+checkpoint2 = 'bert-base-uncased'
+#Download vocabulary the same as the one model pretrained with
+tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+"""
+How tokenizer works
+"""
+sequence = "Let's have a chat"
+#This tokenizer is a subword tokenizer: it splits the words until it obtains tokens 
+# that can be represented by its vocabulary.
+tokens = tokenizer.tokenize(sequence)
+
+#get numberical representation of word from dictionary
+input_ids = tokenizer.convert_tokens_to_ids(tokens)
+print(input_ids)
+
+#Add special starting and ending sentence to tokens
+final_input = tokenizer.prepare_for_models(input_ids)
+
+"""
+Short of tokenizer
+"""
+final_tokens = tokenizer('Let\'s have a chat')
+print(final_tokens)
+#Note that attention mask use to indicate which tokens are padding and should not be important
+#Use attention mask with flag: tokenizer(sequences, padding = True)
